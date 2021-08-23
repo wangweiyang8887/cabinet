e5977853c99fd9614a9695dd3614edbb1bd4a5c3
evan <98708887@qq.com>
2021-08-23 17:37:02 +0800

    add base formatter and timer

--- cabinet/CurrentDateVC.swift
+++ cabinet/CurrentDateVC.swift
@@ -7,6 +7,8 @@ class CurrentDateVC : BaseViewController {
     @IBOutlet private var dateLabel: UILabel!
     @IBOutlet private var closeButton: UIButton!
     
+    private var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cabinetWhite
@@ -15,6 +17,17 @@ class CurrentDateVC : BaseViewController {
         let value = UIInterfaceOrientation.landscapeLeft.rawValue
         UIDevice.current.setValue(value, forKey: "orientation")
         closeButton.addTapHandler { [unowned self] in self.navigationController?.popViewController(animated: true) }
+        getCurrentDateFormatter()
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+            self?.getCurrentDateFormatter()
+        }
+        RunLoop.current.add(timer!, forMode: .common)
+    }
+    
+    private func getCurrentDateFormatter() {
+        let date = Date()
+        timeLabel.text = date.cabinetTimeDateFormatted()
+        dateLabel.text = date.cabinetShortTimelessDateFormatted()
     }
     
     override func viewWillAppear(_ animated: Bool) {
