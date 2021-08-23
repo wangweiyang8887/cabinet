22b0658d1a44a19116d153539908ffc789d822e9
evan <98708887@qq.com>
2021-08-23 15:58:24 +0800

    add home page

---
+++ cabinet/BaseViewController.swift
@@ -0,0 +1,12 @@
+// Copyright © 2021 evan. All rights reserved.
+
+import UIKit
+
+class BaseViewController : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Cabinet"
+        view.backgroundColor = .white
+    }
+}
---
+++ cabinet/HomePageVC.swift
@@ -0,0 +1,10 @@
+// Copyright © 2021 evan. All rights reserved.
+
+import UIKit
+
+class HomePageVC : BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
